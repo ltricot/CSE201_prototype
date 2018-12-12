d03ecaef9d5f55591d56bf39c09dde0f522c79c8
Abdelrahmansameh <45060699+Abdelrahmansameh@users.noreply.github.com>
2018-12-12 22:49:34 +0100

    same key mistake... someone kill me please

--- data_engineering/Source.cpp
+++ data_engineering/Source.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 
 int main() {
-	/*const utility::string_t storage_connection_string = utility::conversions::to_string_t("DefaultEndpointsProtocol=https;AccountName=abdelrahmansameh;AccountKey=0KyoXM2pAEIePW34e0zQkEV+M1PDwPs7RdSe1+aT1xo9ZZXCyaGv+tdAYp0rCFkVcq8L/hKCh6puen7PVsihkQ==;EndpointSuffix=core.windows.net");
+	/*const utility::string_t storage_connection_string = utility::conversions::to_string_t("");
 	
 	// Retrieve the storage account from the connection string.
 	azure::storage::cloud_storage_account storage_account = azure::storage::cloud_storage_account::parse(storage_connection_string);
