87768ae6ced80fd3d55aa31d6916099c2dcb445a
Abdelrahmansameh <45060699+Abdelrahmansameh@users.noreply.github.com>
2018-12-12 22:44:40 +0100

    Added the matrix and referencesreader classes

--- data_engineering/Source.cpp
+++ data_engineering/Source.cpp
@@ -4,11 +4,13 @@
 #include <string>
 #include "Paper.h"
 #include "PapersReader.h"
+#include "ReferencesReader.h"
+
 using namespace std;
 
 
 int main() {
-	const utility::string_t storage_connection_string = utility::conversions::to_string_t("INSERT KEY HERE");
+	/*const utility::string_t storage_connection_string = utility::conversions::to_string_t("DefaultEndpointsProtocol=https;AccountName=abdelrahmansameh;AccountKey=0KyoXM2pAEIePW34e0zQkEV+M1PDwPs7RdSe1+aT1xo9ZZXCyaGv+tdAYp0rCFkVcq8L/hKCh6puen7PVsihkQ==;EndpointSuffix=core.windows.net");
 	
 	// Retrieve the storage account from the connection string.
 	azure::storage::cloud_storage_account storage_account = azure::storage::cloud_storage_account::parse(storage_connection_string);
@@ -21,6 +23,9 @@ int main() {
 	azure::storage::cloud_table table = table_client.get_table_reference(utility::conversions::to_string_t(s));
 	
 	PapersReader R("test.txt");
-	R.UploadPapers(table);
+	R.UploadPapers(table);*/
+	
+	ReferencesReader r("References.txt");
+	r.MakeMatrix();
 	return 0;
 }
