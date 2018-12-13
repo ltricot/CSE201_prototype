5f64a96baaf4c21a3e9732d8aad13cefd6f20381
Abdelrahmansameh <45060699+Abdelrahmansameh@users.noreply.github.com>
2018-12-13 08:16:14 +0100

    Added documentation for matrix.h

--- data_engineering/Matrix.h
+++ data_engineering/Matrix.h
@@ -4,9 +4,10 @@
 using namespace std;
 
 class Matrix {
+/*Adjacency matrix stored in a file system. Every row is stored in a .txt files and contains the entries where it is 1. */
 public:
 	string root_directory;
 	Matrix(string dir);
-	int read(long i, long j);
-	void write(long i, long j);
+	int read(long i, long j); /* Returns the i,j'th entry of the Matrix*/
+	void write(long i, long j); /*Sets the i,j'th entry to 1 */
 };
