f5c176d6f0e11cc000bf4a862137b123bb88c1e7
Abdelrahmansameh <45060699+Abdelrahmansameh@users.noreply.github.com>
2018-12-13 08:26:32 +0100

    Added documentation for PapersReader.h

--- data_engineering/PapersReader.h
+++ data_engineering/PapersReader.h
@@ -4,8 +4,9 @@
 #include <string>
 
 class PapersReader : public Reader {
+/* Specialized reader for a file containing information on a list of research papers */ 
 public:
-	std::string filename;
-	PapersReader(std::string name);
-	void UploadPapers(azure::storage::cloud_table& table);
+	std::string filename; /*The file name */
+	PapersReader(std::string name); 
+	void UploadPapers(azure::storage::cloud_table& table); /* Creates a Paper object from each line and uploads it */
 };
