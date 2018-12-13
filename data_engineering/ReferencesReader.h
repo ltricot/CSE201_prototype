c39e430ebd58270781f74e96dc57916c95e5431f
Abdelrahmansameh <45060699+Abdelrahmansameh@users.noreply.github.com>
2018-12-13 08:39:29 +0100

    Added documentation for Reader.h

--- data_engineering/ReferencesReader.h
+++ data_engineering/ReferencesReader.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "Reader.h"
 class ReferencesReader : public Reader {
+/* Specialized reader which reads a file containing the edges of a graph, then creates and stores the adjacency matrix of that graph */ 
 public:
 	std::string filename;
 	ReferencesReader(std::string name);
-	void MakeMatrix();
+	void MakeMatrix(); /*Read the file and makes the matrix */
 };
