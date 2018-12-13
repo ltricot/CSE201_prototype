2de0fbbe26a06fb3975770974f032efec6b1fdf3
Abdelrahmansameh <45060699+Abdelrahmansameh@users.noreply.github.com>
2018-12-13 08:36:22 +0100

    Added documentation for Reader.h

--- data_engineering/Reader.h
+++ data_engineering/Reader.h
@@ -3,13 +3,14 @@
 #include <vector>
 
 class Reader {
+/*Object which reads a table stored in a file*/
 public:
-	std::string filename;
-	std::string delim;
+	std::string filename; /*The file's name */
+	std::string delim; /* What delimits the columns */
 	Reader();
-	Reader(std::string name);
-	Reader(std::string name, std::string delimeter);
-	std::vector<std::vector<std::string>> read();
-	void print();
+	Reader(std::string name); /*By default the delimeter is a comma */
+	Reader(std::string name, std::string delimeter); 
+	std::vector<std::vector<std::string>> read(); /*Returns a matrix of the entries in the .txt file*/
+	void print(); 
 };
 
