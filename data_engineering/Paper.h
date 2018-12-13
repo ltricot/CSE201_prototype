c99e63e7bbf219b0f9dc658652ff7ccd58818b03
Abdelrahmansameh <45060699+Abdelrahmansameh@users.noreply.github.com>
2018-12-13 08:21:47 +0100

    Added documentation for Paper.h

--- data_engineering/Paper.h
+++ data_engineering/Paper.h
@@ -5,13 +5,14 @@
 #include <vector>
 using namespace std;
 class Paper {
+/* Basically a wrapper class for azure table entitities */
 public:
-	int date;
-	int id;
-	string title;
-	string author;
-	azure::storage::table_entity entity;
+	int date; /*Year of publication of the paper*/
+	int id; /*Unique identifier of the paper, also its index in the adjacency matrix*/
+	string title; /* Title of the paper*/
+	string author; /*Authot of the paper */
+	azure::storage::table_entity entity; /*Azure table entry */
 	Paper(string tit, string a, string top, int d, int id);
-	Paper(vector<string> line);
-	azure::storage::table_result upload(azure::storage::cloud_table& table);
+	Paper(vector<string> line); /*Constructor for paper object from a line in the Papers.txt file from the Microsoft Academic Graph */
+	azure::storage::table_result upload(azure::storage::cloud_table& table); /*Upload the paper on an Azure table */
 };
