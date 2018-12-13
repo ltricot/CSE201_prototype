04435816c68df9c79fb9c0a217a269b306641642
Loan Tricot <ltricot@gmail.com>
2018-12-13 09:28:36 +0100

    matfac header for deivis

---
+++ Recommendation Algorithms/matfac/factorization.hpp
@@ -0,0 +1,56 @@
+#include <string>
+
+#include <Eigen/core>
+
+
+class Author {
+    public:
+    std::string name;
+}
+
+class Paper {
+    public:
+    std::string id;
+}
+
+typedef std::pair<Author, Paper> Edge;
+
+
+class Factorizer {
+    private:
+    // row matrices
+    typedef std::vector<Eigen::RowVectorXf> vectors;
+    vectors authors;
+    vectors papers;
+    vectors tfidf;
+
+    // don't worry about this right now
+    std::map<Author, int> authorIndex;
+    std::map<Paper, int> paperIndex;
+
+    public:
+    // initialize authors & papers randomly
+    // store tfidf
+    Factorizer(vectors tfidf);
+
+    // each argument is an index in your matrices
+    void update(int author, int paper, float lr);
+
+    // update over iterable
+    template <class Iterable> void train(Iterable data) {
+        Iterable::iterator it;
+        for (it = data.begin(); it != data.end(); it++) {
+            // call update; ``*it`` is an Edge
+        }
+    }
+};
+
+
+/*
+ * std::vector<int> numbers;
+ * std::vector<int>::iterator i;
+ *
+ * for (i = numbers.begin(); i != numbers.end(); i++) {
+ *     std::cout << *i << std::endl;
+ * }
+ */
