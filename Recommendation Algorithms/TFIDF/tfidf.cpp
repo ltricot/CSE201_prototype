449d98661fbeb79cb3326f5637e6cc6af9d7b404
Loan Tricot <ltricot@gmail.com>
2018-12-20 09:35:55 +0100

    added comments to tfidf methods

--- Recommendation Algorithms/TFIDF/tfidf.cpp
+++ Recommendation Algorithms/TFIDF/tfidf.cpp
@@ -6,13 +6,22 @@ void tfidf::createVocabList() {
 	
 		for (std::string word : document) { //iterate over the words in the document
 			AllWords.insert(word); //insert all the words in AllWords
-			} 
-		}
+		} 
+	}
 	/*create VocabList by removing all the repetitions in the set AllWords*/
 	std::copy(AllWords.begin(), AllWords.end(), std::back_inserter(vocabList)); 
-	}
+}
 
- Eigen::VectorXd tfidf::countOccDoc(const std::vector<std::string> & doc){
+/* A small comment concerning complexity:
+ * Inside the loop over each word is an implicit loop over the vocabList to find the word's index.
+ * This makes it so the complexity of this function is O(nm), where n is the document length and
+ * m is the length of the vocabList.
+ *
+ * A solution to this would be to use a map from words to indices instead of a std::vector to
+ * store the vocabList, which would make the complexity O(n). One can also iterate over a map's
+ * keys so you wouldn't lose functionality.
+ */
+Eigen::VectorXd tfidf::countOccDoc(const std::vector<std::string> & doc) {
 	Eigen::VectorXd res = Eigen::VectorXd::Zero(vocabList.size()); //create an array of size VocabList full of zero
 	
 	for (std::string word : doc) { //iterate over the words in the input document
@@ -20,6 +29,7 @@ void tfidf::createVocabList() {
 		if (i < vocabList.size()) 
 			res(i) += 1; //add one occurence of the word 
 	}
+
 	return res; 
 	/*return a vector whose coeff i correspond to the number of occurences in the doc of 
 	the word at index i in vocabList*/
@@ -56,11 +66,17 @@ void tfidf::createCountDoc() {
 	dataMat.resize(0,0);
 }
 
+/* This is where you need to take advantage of Eigen.
+ * Find a way to reduce your loops to matrix operations and call the corresponding
+ * Eigen methods on your matrices / vectors.
+ * 
+ * Eigen does the looping itself and does it much (much!) faster.
+ */
 void tfidf::calweightMat() {
 	createVocabList();
 	createOccMat();
 	createCountDoc();
-	
+
 	weightMat.resize(nrow, ncol);
 	for (unsigned int i = 0; i != nrow; ++i)
 	{
@@ -70,11 +86,5 @@ void tfidf::calweightMat() {
 			double idf = log((double)nrow / (CountDoc(j)) ); //compute the inverse doc frequency
 			weightMat(i,j) = tf * idf; // TF-IDF equation
 		}
-		
 	}
-	
-
-
 }
-
-
