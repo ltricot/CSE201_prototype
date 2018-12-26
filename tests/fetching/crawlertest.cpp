8083e84d2ecfc7d60db8fd04e80b29ef9b68a819
Loan Tricot <ltricot@gmail.com>
2018-12-27 00:04:40 +0100

    added formatter

--- tests/fetching/crawlertest.cpp
+++ tests/fetching/crawlertest.cpp
@@ -1,14 +1,14 @@
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <string>
-#include <iostream>
 
+#include "crawler.hpp"
 #include "kortchemski.cpp"
 #include "testmain.hpp"
-#include "crawler.hpp"
 
-
-TEST_CASE("fetching/crawler/getPairs", "test whether XML is well parsed to fetch author / paper edges") {
+TEST_CASE("fetching/crawler/getPairs",
+          "test whether XML is well parsed to fetch author / paper edges") {
     std::vector<Edge> edges;
     Crawler crawler;
 
@@ -20,33 +20,18 @@ TEST_CASE("fetching/crawler/getPairs", "test whether XML is well parsed to fetch
 
 TEST_CASE("fetching/crawler/getID", "test getID on different arxiv urls") {
     std::vector<std::string> urls = {
-        "http://arxiv.org/abs/0804.0446v3",
-        "http://arxiv.org/abs/1106.0271v4",
-        "http://arxiv.org/abs/1109.4138v3",
-        "http://arxiv.org/abs/1110.2163v4",
-        "http://arxiv.org/abs/1205.3145v4",
-        "http://arxiv.org/abs/1312.4933v2",
-        "http://arxiv.org/abs/1312.5678v3",
-        "http://arxiv.org/abs/1504.04358v2",
-        "http://arxiv.org/abs/1307.6818v3",
-        "http://arxiv.org/abs/1412.1068v2"
-    };
+         "http://arxiv.org/abs/0804.0446v3", "http://arxiv.org/abs/1106.0271v4",
+        "http://arxiv.org/abs/1109.4138v3", "http://arxiv.org/abs/1110.2163v4",
+        "http://arxiv.org/abs/1205.3145v4", "http://arxiv.org/abs/1312.4933v2",
+        "http://arxiv.org/abs/1312.5678v3", "http://arxiv.org/abs/1504.04358v2",
+        "http://arxiv.org/abs/1307.6818v3", "http://arxiv.org/abs/1412.1068v2"};
 
-    std::vector<std::string> ids = {
-        "0804.0446v3",
-        "1106.0271v4",
-        "1109.4138v3",
-        "1110.2163v4",
-        "1205.3145v4",
-        "1312.4933v2",
-        "1312.5678v3",
-        "1504.04358v2",
-        "1307.6818v3",
-        "1412.1068v2"
-    };
+    std::vector<std::string> ids = {"0804.0446v3", "1106.0271v4", "1109.4138v3", "1110.2163v4",
+                                    "1205.3145v4", "1312.4933v2", "1312.5678v3", "1504.04358v2",
+                                    "1307.6818v3", "1412.1068v2"};
 
     // ugly iteration :(
-    for(int i = 0; i != urls.size(); i++) {
+    for (int i = 0; i != urls.size(); i++) {
         REQUIRE(getID(urls[i]) == ids[i]);
     }
 }
