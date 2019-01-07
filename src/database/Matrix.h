90e9fcff0c4e82690fa33d1d6f659fed7bf9dbce
abdelrahmansameh <abdelrahmansameh129@gmail.com>
2019-01-07 13:31:05 +0100

    Added some documentation

--- src/database/Matrix.h
+++ src/database/Matrix.h
@@ -4,13 +4,63 @@
 using namespace std;
 
 class Matrix {
-/*Adjacency matrix stored in a file system. Every row is stored in a .txt files and contains the entries where it is 1. */
+/** @brief Adjacency matrix stored in a file system. 
+ * 
+ * @details Adjacency Matrix class. The idea is to construct a directory in the form of a tree structure.
+ *	Every row is stored in a file row.txt which contains the non-zero entries of that row. The path to the
+ *  file is obtained by first padding the row number with zeroes on the left, cutting it up into  substrings
+ *  of length three, then appending them, for example, to store the 00123184945'th row, we would create the 
+ *  file 001/231/849/45.txt  Each line in the text file represents one edge and follows the following format: 
+ * "column, weight" */
 public:
+
+	//Stores the root directory for the files of the matrix
 	string root_directory;
+
 	Matrix();
 	Matrix(string dir);
-	int read(string i, string j); /* Returns the i,j'th entry of the Matrix*/
+
+	/** @brief Read the matrix entries
+	 * 
+	 * @details Recovers the path to the text file for the i'th line, then looks for j in it. If it finds it returns
+	 * the associated weight, if not returns 0.
+	 * 
+	 * @param i the row as a string in case it is too big to be stored in a long
+	 * 
+	 * @param j the column as a string in case it is too big to be stored in a long
+	 * 
+	 *@return the value of the i,j'th entry 
+	 * */
+	int read(string i, string j); 
+
+	/**
+	 * @param i the row
+	 * 
+	 * @param j the column
+	 * */
 	int read(long long i, long long j);
-	void write(long i, long j, int val); /*Sets the i,j'th entry to val */
+	
+	/** @bried Modify the matrix
+	 * 
+	 * @details Recovers the path to the text file of the i'th line, then looks for j in it. If it finds it, 
+	 * modifies the value of the weight to val, if not adds it at the end of the file with val as the weight. 
+	 * 
+	 * @param i the row as a string in case it is too big to be stored in a long
+	 * 
+	 * @param j the column as a string in case it is too big to be stored in a long
+	 * 
+	 * @param val the new value of the i,j'th entry
+	 * 
+	 * 
+	 * */
+	void write(long i, long j, int val); 
+	/**
+	 * @param i the row
+	 * 
+	 * @param j the column
+	 * 
+	 * @param val the new value of the i,j'th entry
+	 * 
+	 * */
 	void write(string i, string j, int val);
 };
--- src/database/Paper.h
+++ src/database/Paper.h
@@ -4,15 +4,48 @@
 #include <string>
 #include <vector>
 using namespace std;
+/**
+ *  @brief Basically a wrapper class for azure table entitities */
 class Paper {
-/* Basically a wrapper class for azure table entitities */
+
 public:
-	int date; /*Year of publication of the paper*/
-	int id; /*Unique identifier of the paper, also its index in the adjacency matrix*/
-	string title; /* Title of the paper*/
-	string author; /*Authot of the paper */
-	azure::storage::table_entity entity; /*Azure table entry */
+	//Year of publication of the paper
+	int date; 
+
+	// Unique identifier of the paper, also its index in the adjacency matrix
+	int id; 
+	
+	// Title of the paper
+	string title; 
+	
+	//Author of the paper
+	string author; 
+	
+	//Azure table entry
+	azure::storage::table_entity entity; 
+	/** @brief Constructor for paper object
+	 * 
+	 * @param tit Title of the paper
+	 * 
+	 * @param a Author of the paper
+	 * 
+	 * @param top Topic
+	 * 
+	 * @param d Date 
+	 * 
+	 * @param id Unique identifier of the paper */
 	Paper(string tit, string a, string top, int d, int id);
-	Paper(vector<string> line); /*Constructor for paper object from a line in the Papers.txt file from the Microsoft Academic Graph */
-	azure::storage::table_result upload(azure::storage::cloud_table& table); /*Upload the paper on an Azure table */
-};
+	
+	/** @brief Constructor for paper object from a line in the Papers.txt file from the Microsoft Academic Graph */
+	Paper(vector<string> line); 
+	
+	/** @brief Upload the paper to an Azure table
+	 * 
+	 * @details 
+	 *
+	 * @param table reference to the azure table on which the paper information will be stored 
+	 * 
+	 * @return The outcome of the upload operation 
+	 * */
+	azure::storage::table_result upload(azure::storage::cloud_table& table); 
+}
