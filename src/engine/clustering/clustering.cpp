1dcd886f357f2dfde05cb4a836cc1ce97867fa3a
MarineHoche <45051038+MarineHoche@users.noreply.github.com>
2019-01-13 16:38:21 +0100

    Update clustering.cpp

--- src/engine/clustering/clustering.cpp
+++ src/engine/clustering/clustering.cpp
@@ -154,21 +154,3 @@ void cluster::createcluster( std::vector<Eigen::VectorXd> & input){
     }
 }
 
-/*
-int main(){
-    srand(time(0)); //make a seed that will be use to generate random numbers
-    //don't know how to give the input of the form std::vector<Eigen::VectorXi> to the algorithm
-    cluster res(input);
-    std::vector <int> labelres= res.label;
-    std::vector<int> listlabel; //list of all the labels
-    for (int v=0; v<labelres.size(); v++) {
-        if(std::find(listlabel.begin(), listlabel.end(), labelres[v]) == listlabel.end()) {
-            listlabel.push_back(labelres[v]);
-        }
-            std::cout<<"the node "<<v<< "has label"<< labelres[v]<<std::endl;
-    }
-    std::cout<<"the number of clusters is"<<listlabel.size()<<std::endl; //to know how many clusters we have created
-
-    return 0;
-
-}*/
