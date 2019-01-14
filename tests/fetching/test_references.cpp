acb53b0ef9c4864d0d91b65ed3ec8234ac7b8377
yassine280 <45051834+yassine280@users.noreply.github.com>
2019-01-14 14:39:42 +0100

    Update test_references.cpp

--- tests/fetching/test_references.cpp
+++ tests/fetching/test_references.cpp
@@ -3,9 +3,8 @@
 TEST_CASE("fetching/references/readFile,fetching/references/writeFile", "test if it reads what we write exactly in the same way"){
     std::string filename;
     filename ="test";
-    std::map<std::string;
+    std::map<std::string, std::vector<std::string>> data ;
 
-    std::vector<std::string>> data;
     data = <"Loan is cool", "maria as well">;
 
 
