fdbcffaf023e2b3d903a901821889a7093e5428a
yassine280 <45051834+yassine280@users.noreply.github.com>
2019-01-14 14:34:11 +0100

    Create test_references.cpp
    Not correct, has to be completed

---
+++ tests/fetching/test_references.cpp
@@ -0,0 +1,13 @@
+TEST_CASE("fetching/references/readFile,fetching/references/writeFile", "test if it reads what we write exactly in the same way"){
+    std::string filename;
+    filename ="test";
+    std::map<std::string;
+
+    std::vector<std::string>> data;
+    data = <"Loan is cool", "maria as well">;
+
+
+    
+    REQUIRE(references.writeFile(data, std::map<std::string, filename) == references.readFile(filename)
+}
+//Not finished and not correct
