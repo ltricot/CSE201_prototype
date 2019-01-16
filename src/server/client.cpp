d6f784ffa31c4dde2eeeb1f6508c73733cc71d15
Antonin Jean <antoninjean@MacBook-de-Antonin.local>
2019-01-16 23:16:13 +0100

    ngs added to client class

--- src/server/client.cpp
+++ src/server/client.cpp
@@ -16,28 +16,60 @@ std::vector<std::string> Client::getTopics(){
     for(json::iterator it = resp.begin(); it != resp.end(); it++) {
         ret.push_back(*it);
     }
+    return ret;
 }
 
 std::vector<std::string> Client::getLikes(Author author) {
-    return get(ip + /users/:id/likes);
+    std::string response = get(ip + "/users/:id/likes");
+    json resp = json::parse(response);
+
+    std::vector<std::string> ret;
+    for(json::iterator it = resp.begin(); it != resp.end(); it++) {
+        ret.push_back(*it);
+    }
+    return ret;
 }
 
 bool Client::putLikes(Author author, std::vector<std::string> topics){
-    return post(ip + /users/:id/likes);
+    std::string response =  post(ip + /users/:id/likes);
+    json resp = json::parse(response);
+    if (resp.success == 1) {
+        return true
+    }
+    else {
+        return false
+    }
 }
 
 Paper Client::getRecommendation(Author author) {
-    return get(ip + /users/:id/recommendation);
+    std::string response = get(ip + /users/:id/recommendation);
+
 }
 
 Paper Client::getSummary(Paper paper) {
     return get(ip + /);
 }
 
-std::vector<std::string> Client::getArticles(Author author){
-    return get(ip + /users/:id/articles);
+std::vector<std::string> Client::getArticles(Author author) {
+     
+    std::string response = get(ip + "/users/:id/articles");
+    json resp = json::parse(response);
+
+    std::vector<std::string> ret;
+    for(json::iterator it = resp.begin(); it != resp.end(); it++) {
+        ret.push_back(*it);
+    }
 }
 
 bool Client::putArticles(std::vector<std::string> articles){
-    return post(ip + /users/:id/articles)
+    std::string response = post(ip + /users/:id/articles)
+    json resp = json::parse(response);
+    if (resp.success == 1) {
+        return true
+    }
+    else {
+        return false
+    }
 }
+
+// hello world
