22bcc09129fc408b1e2d372525d0d8b81c8d014a
Antonin Jean <antoninjean@MacBook-de-Antonin.local>
2019-01-16 23:10:25 +0100

    real implementation of the client class

--- src/server/client.cpp
+++ src/server/client.cpp
@@ -1,56 +1,43 @@
 #include "client.hpp"
+#include "tools.cpp"
+
+#include <vector>
 #include <string>
+
 
 Client::Client(std::string ip, int port) : ip(ip), port(port) {
 }
 
 std::vector<std::string> Client::getTopics(){
-    std::vector<std::string> test_topics = {
-        "physics", "math", "computers"
-    };
-    return test_topics;
+    std::string response = get(ip + "/topics");
+    json resp = json::parse(response);
+
+    std::vector<std::string> ret;
+    for(json::iterator it = resp.begin(); it != resp.end(); it++) {
+        ret.push_back(*it);
+    }
 }
 
-std::vector<std::string> Client::getLikes(Author author){
-    std::vector<std::string> test_string = {"physics", "math", "computers"};
-
-    return test_string;
+std::vector<std::string> Client::getLikes(Author author) {
+    return get(ip + /users/:id/likes);
 }
 
 bool Client::putLikes(Author author, std::vector<std::string> topics){
-    return true;
+    return post(ip + /users/:id/likes);
 }
 
 Paper Client::getRecommendation(Author author) {
-    Paper test_paper("1812.01234_v2");
-    return test_paper;
+    return get(ip + /users/:id/recommendation);
 }
 
 Paper Client::getSummary(Paper paper) {
-    paper.summary = R"(Consumer electronic (CE) devices increasingly rely on wireless local area
-networks (WLANs). Next generation WLANs will continue to exploit multiple
-antenna systems to satisfy the growing need for WLAN system capacity.
-Multiple-input multiple-output (MIMO) antenna systems improve the spectral
-efficiency and single user throughput. Multi-user MIMO (MU-MIMO) systems
-exploit the spatial separation of users for increasing the sum-throughput. In
-an MU-MIMO system, efficient channel sounding is essential for achieving
-optimal performance. The system analysis in this paper provides insights into
-the rate at which to perform channel sounding. This paper shows that optimal
-sounding intervals exist for single user transmit beamforming (SU-TxBF) and
-MU-MIMO, and proposes a low-complexity dynamic sounding approach for practical
-MU-MIMO WLAN deployments. The proposed approach adjusts the sounding interval
-adaptively based on the real-time learning outcomes in the given radio
-environment. Using real over-the-air channel measurements, significant
-throughput improvements (up to 31.8%) are demonstrated by adopting the proposed
-dynamic sounding approach, which is compliant with IEEE 802.11ac.)";
-    return paper;
+    return get(ip + /);
 }
 
 std::vector<std::string> Client::getArticles(Author author){
-    std::vector<std::string> test = {"1812.01234_v2", "1901.03532", "1901.03680"};
-    return test;
+    return get(ip + /users/:id/articles);
 }
 
 bool Client::putArticles(std::vector<std::string> articles){
-    return true;
+    return post(ip + /users/:id/articles)
 }
