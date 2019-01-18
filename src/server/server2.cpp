7209c0599faed0d21e8430636204b429fad08842
Smakson <mihasmaka1@gmail.com>
2019-01-18 21:11:57 +0100

    Just a new version of the server
    Updated the dependencies so I can compile it, just need to get it to the VM

--- src/server/server2.cpp
+++ src/server/server2.cpp
@@ -1,11 +1,12 @@
 // @author Miha Smaka
+#include <pistache/http.h>
 #include <pistache/endpoint.h>
+#include <pistache/router.h>
 #include <iostream>
 #include <string>
 #include <sstream>
 #include "../database/driver.hpp"
-#include <fstream>
-#include "Reader.h"
+
 
 using namespace Pistache;
 
@@ -17,10 +18,9 @@ using json = nlohmann::json;
 class GUI_Serv {
 
     GUI_Serv(std::string d, Address addr){
-        httpEndpoint(std::make_shared<Http::Endpoint(addr));
+        httpEndpoint(std::make_shared<Http::Endpoint>(addr));
         dir = d;
     }
-	
 	std::string dir;
 
 	Rest::Router router;
@@ -58,8 +58,8 @@ class GUI_Serv {
 
 	void getLikes(const Rest::Request& request, Http::ResponseWriter response) {
 		auto id = request.param(":id").as<int>;
-		std::vector<std::string> likes = getUserLikes(id)
-		//js = "{\"physics\" : 0, \"math\": 1, \"computers\": 1}"; //answer for testing response
+		//js = getUserLikes(id)
+		js = "{\"physics\" : 0, \"math\": 1, \"computers\": 1}"; //answer for testing response
 		response.send(Http::Code::Ok, js);
 	}
 
@@ -94,15 +94,16 @@ class GUI_Serv {
 		for (json::iterator it = d.begin(); it != d.start(); it++){
 			out.push_back(*it);
 		}
-		bool succ = putUserLikes(id, out);
-		if (succ) {
+		/*TO BE IMPLEMENTED
+		bool suc = putUserLikes(id, out, dir);
+		if (suc) {
 			response.send(Http::Code::Ok, "{\"success\": 1}");
 			return;
 		}
 		else{
 			response.send(Http::Code::Not_Found, "{\"failure\": 0}" )
 			return;
-		}
+		}*/
 		response.send(Http::Code::Ok, "{\"success\": 1}");
 	}
 
@@ -128,76 +129,6 @@ class GUI_Serv {
 	}
 	
 };
-/**@brief Returns the user's liked topics
- * 
- * @details Finds the user's text file and returns the topics it contains in the form of a vector
- * of strings
- * 
- * @param id the User's id
- * 
- * @return vector of topics
- */ 
-std::vector<std::string> getUserLikes(std::string id){
-	Reader r(id+".txt")
-	std::vector<std::vector<std::string>> tmp = r.read();
-	std::vector<std::string> ret;
-	for (std::vector<std::vector<std::string>>>::iterator it = tmp.begin(); it != tmp.end(); it ++){
-		ret.push_back(*it[0]);
-	}
-	return ret;
-}
-
-/**@brief Calls putUserLike(id, like) on each like in likes
- */
-bool putUserLikes(std::string id, std::vector<std::string> likes){
-	for(std::vector<std::string>::iterator it = likes.begin(); it != likes.end(); it++){
-		bool b = putUserLike(id, *it);
-	}
-	return true;
-}
-
-/**@brief Records the fact that the user liked this topic
- * 
- * @details Creates or opens a text file with the user's id, then adds the like
- * 
- * @param id the User's id
- * 
- * @param like a topic liked by the user
- */
-bool putUserLike(std::string id, std::string like){
-	std::string filename = id + ".txt";
-	ifstream inp(filepath);
-	ofstream out(id  + "tmp.txt");
-
-	string line;
-	bool liked = false;
-	while (getline(inp, line)) {
-			vector<string> vec;
-			boost::algorithm::split(vec, line, boost::is_any_of(","));
-			if (vec[0] == like) {
-				out << line << "\n";
-				liked = true;
-			}
-			else {
-				out << line << "\n";
-			}
-		}
-	}
-	inp.close();
-
-	if (liked) {
-		out.close();
-		remove(filepath.c_str());
-		rename((id + "tmp.txt").c_str(), filepath.c_str());
-		return true;
-	}
-
-	out << like << "\n";
-	out.close();
-	remove(filepath.c_str());
-	rename((id + "tmp.txt").c_str(), filepath.c_str());
-	return true;
-}
 
 std::string jsonize(std::vector<std::string>& arts) {
 	std::string output = "{";
@@ -210,16 +141,6 @@ std::string jsonize(std::vector<std::string>& arts) {
 	return output;
 }
 
-/**@brief Recovers the articles that the user liked. 
- * 
- * @details Goes into the interaction matrix and returns the row corresponding to the user's id
- * 
- * @param id The user's id
- *
- * @param dr The directory of the interaction matrix
- * 
- * @return A vector containing the ids of the papers associated to the user
- */
 std::vector<std::string> getUserArticles(std::string id, std::string dr){
 	Author u(id);
 	Driver d(dr);
@@ -230,17 +151,6 @@ std::vector<std::string> getUserArticles(std::string id, std::string dr){
 	}
 	return ret;
 }
-
-/** @brief Records the fact that the user liked these articles
- * 
- * @details Adds an edge in the interaction matrix between the user and every papaper
- * 
- * @param id the User's id
- * 
- * @param articles Vector of articles
- * 
- * @param dr The interaction matrix's directory
- */
 bool putUserArticles(std::string id, std::vector<std::string> articles, std::string dr){
 	Author u(id);
 	Driver d(dr);
@@ -248,7 +158,6 @@ bool putUserArticles(std::string id, std::vector<std::string> articles, std::str
 		bool b = d.writeEdge(Edge(u, Paper(*it)));
 	}
 	return true;
-}
 
 
 
