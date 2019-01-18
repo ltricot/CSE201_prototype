db4fdfa2e433d2d8688e7ee9b693c6ed411b1928
Ghjulia Sialelli <g.sialelli@hotmail.fr>
2019-01-18 21:20:09 +0100

    executable for TFIDF

--- src/executables/runtfidf.cpp
+++ src/executables/runtfidf.cpp
@@ -6,9 +6,24 @@
 namespace fs = std::experimental::filesystem;
 
 int main(int argc, char *argv[]){
-    // entree : sdata
-    // sortie : tfidfdata 
+    std::string sdata = std::string(argv[1]);  // name of the driver
+                    // filled by minhash, from which we read
 
+    std::string tfidfdata = std::string(argv[2]); // name of the driver
+                    // we must create then fill using tfidf
+
+    Driver dtfidf(tfidfdata);
+
+    TFIDF tfidf(sdata);
+    for(TFIDF::iterator ite(*tfidf) ){
+        pEdge edge = *ite ;
+        Author word = Author(std::get<0>(edge));    // sorry for this abomination
+        Paper paper = std::get<1>(edge);
+        double weight = std::get<2>(edge);
+        Edge newEdge(word,paper,edge);
+
+        dtfidf.writeEdge(newEdge);
+    }
 }
 
 
