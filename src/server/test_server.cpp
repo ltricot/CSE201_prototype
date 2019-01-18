4d38ffcec8fd9be25b88ba4dddc29b99ad6f6e20
Smakson <mihasmaka1@gmail.com>
2019-01-18 15:54:06 +0100

    Added a new version of the server and a test.
    Good luck compiling, I have legit no clue how to do this. The includes are probably screwed up, should be corrected later today.

---
+++ src/server/test_server.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "client.hpp"
+#include "driver.hpp"
+}
+
+//40.89.156.133
+
+void print_vect(std::vector<std::string> vect) {
+	std::vector<std::string>::iterator it;
+	for (it = vect.begin(); it != vect.end(); it++) {
+		std::cout << *it << std::endl;
+	}
+}
+
+int main() {
+
+    std::string ip = "40.89.156.133";
+	Client::Client MyC(ip, 80);
+	std::vector<std::string> tops = MyC.getTopics();
+	Author testy = "Testy McTestface";
+	std::vector<std::string> getliks = MyC.getLikes(testy);
+	std::vectore<std::string> topics;
+	topics.push_back("SEND NUDES");
+	topics.push_back("PLEASE");
+	topics.push_back("I AM DESPERATE");
+	bool sucliks = MyC.putLikes(testy, topics);
+
+	Paper SUCCC = MyC.getRecommendation(testy);
+
+	std::vector<std::string> arts = MyC.getArticles(testy);
+
+	print_vect(tops);
+
+    return 0;
+}
+
+
+//sudo g++-7 test.cpp -lpistache -pthread -std=c++11
